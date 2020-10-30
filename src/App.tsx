2b5ef64cf8f5f754a2e66b2adb234f674f0225db
rgbsz <rafalgurbisz@outlook.com>
2020-10-30 13:26:11 +0100

    Project setup
    - added styled components with types
    - added materialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col s6'>Hello</div>
+        <div className='col s6'>World</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
